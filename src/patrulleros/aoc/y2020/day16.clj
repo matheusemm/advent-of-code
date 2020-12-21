404fad7f1cc2bc4828cf46ceeb968dcdb6be6304
Matheus <matheus@MBP-de-Markelly.fritz.box>
2020-12-21 12:34:04 +0100

    Solution for day 16 part 2

--- src/patrulleros/aoc/y2020/day16.clj
+++ src/patrulleros/aoc/y2020/day16.clj
@@ -1,5 +1,6 @@
 (ns patrulleros.aoc.y2020.day16
-  (:require [clojure.string :as str]
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
             [patrulleros.aoc.util :as util]))
 
 (def puzzle-input
@@ -18,6 +19,19 @@
    "40,4,50"
    "55,2,20"
    "38,6,12"])
+
+(def example-p2
+  ["class: 0-1 or 4-19"
+   "row: 0-5 or 8-19"
+   "seat: 0-13 or 16-19"
+   ""
+   "your ticket:"
+   "11,12,13"
+   ""
+   "nearby tickets:"
+   "3,9,18"
+   "15,1,5"
+   "5,14,9"])
 
 (defn parse-ticket-values [ticket-line]
   (mapv #(Integer/parseInt %) (str/split ticket-line #",")))
@@ -56,5 +70,53 @@
   ([sdoc]
    (-> sdoc parse-document calculate-ticket-scanning-error-rate)))
 
+(defn valid-ticket? [values validators]
+  (every? (fn [value]
+            (some #(% value) validators))
+          values))
+
+(defn get-valid-tickets [{:keys [fields nearby-tickets]}]
+  (let [validators (map :validator fields)]
+    (filter #(valid-ticket? % validators) nearby-tickets)))
+
+(defn get-fields-values [tickets]
+  (apply mapv vector tickets))
+
+(defn get-validators [{:keys [fields]}]
+  (set (map :validator fields)))
+
+(defn sfirst [xs]
+  (second (first xs)))
+
+(defn decode-fields-order [document]
+  (let [fields-values (-> document get-valid-tickets get-fields-values)
+        fields-cols (->> (for [field (:fields document)
+                               idx (range (count fields-values))
+                               :when (every? (:validator field) (nth fields-values idx))]
+                           [(:name field) idx])
+                         (group-by first)
+                         (map (fn [[name values]]
+                                [name (set (map second values))]))
+                         (sort-by (comp count second) >=))]
+    (reduce (fn [fields [[nname ncols] [name cols]]]
+              (let [col (first (set/difference ncols cols))]
+                (assoc fields col nname)))
+            (sorted-map)
+            (partition 2 1 fields-cols))))
+
+(defn solve-p2
+  ([]
+   (solve-p2 @puzzle-input))
+  ([sdoc]
+   (let [document (parse-document sdoc)
+         departure-fields-indexes (->> (decode-fields-order document)
+                                       (filter #(str/starts-with? (second %) "departure"))
+                                       (map first))]
+     (->> departure-fields-indexes
+          (select-keys (:ticket document))
+          (vals)
+          (apply *)))))
+
 (comment
-  (assert (= 25895 (solve-p1)) "Part 1 solution is wrong."))
+  (assert (= 25895 (solve-p1)) "Part 1 solution is wrong.")
+  (assert (= 5865723727753 (solve-p2)) "Part 2 solution is wrong."))
